5e9700737f5279b6e464179b519bd7c9c3acdb27
Larry-Larriee <lle114@student.hbuhsd.edu>
2023-05-22 23:30:45 -0700

    Minor Changes

---
+++ src/components/Video.js
@@ -0,0 +1,14 @@
+import React, { useRef } from 'react'
+
+export default function Video() {
+
+    const videoRef = useRef(null);
+
+    return (
+        <>
+    
+            
+
+        </>
+    )
+}
