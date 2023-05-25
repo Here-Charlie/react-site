27989999229709d3414368ff6f61d36a9d3d6f65
Larry-Larriee <LarryLe704@gmail.com>
2023-05-24 19:43:30 -0700

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import './App.css';
 import Camera from './components/Camera';
 
 function App() {
+
+  const axios = require("axios");
+  const fs = require("fs");
+
+const image = fs.readFileSync("./logo.svg", {
+    encoding: "base64"
+});
+
+axios({
+    method: "POST",
+    url: "https://detect.roboflow.com/whsnet/3",
+    params: {
+        api_key: "YbQoDWJZi4w5hx63SpTt"
+    },
+    data: image,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+.then(function(response) {
+    console.log(response.data);
+})
+.catch(function(error) {
+    console.log(error.message);
+});
+  
   return (
     <div className="App">
         <Camera />
