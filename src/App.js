3bc9e424afee0b96adf4e005b14abf8347710619
Larry Le <larryle704@gmail.com>
2024-05-02 19:03:06 -0700

    Minor CSS Changes
    Added stying for the header, announcement, and features component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import CharlieLogo from './content/charlie-logo.png';
+import CharlieLogo from './content/charlie-logo.jpg';
 
 import CharlieDetection from './content/charlie-detection.png';
 import OpenVino from './content/openvino-logo.png';
@@ -15,82 +15,85 @@ import Feature from './components/Feature';
 function App() {
 
   return (
+    // the main container holds all the content while the container helps to center the main container
     <div className='container'>
-      <section className='header'>  
-        <div className='header-branding'>
-          <img className='header-image' alt='charlie-logo' src={CharlieLogo} />
-          <p>Here-Charlie</p>
-        </div>
-        <img className='header-image' alt='github logo' src={Github} />
-      </section>
-
-      <main className='content'>
-
-        <section className='content-hero'>
-          <div className='content-hero-text'>
-            <p className='content-hero-text-billboard'>Venture Outwards with A Robotic Service Dog</p>
-            <p className='content-hero-text-subtext'>endorsed by Intel</p>
-          </div>
-
-          <img alt='charlie' src={Charlie} />
-        </section>
-
-        <Announcement
-          header="What's so special about Charlie?"
-          subtext="Our robotic service dog, Charlie, is built to assist those that are visually impaired. Charlie aims to regain functions that were lost due to visual impairment and be affordable." 
-        />
-
-        <Feature 
-          image={CharlieDetection}
-          header={"Equipped With Lidar and Programmed with ROS"}
-          subtext={"so you know he's quick. Charlie can detect still and moving objects at blazing speeds, accurately."}
-        />
-
-        <Feature 
-          image={OpenVino}
-          header={'Trained From Over 10,000 Pictures (and counting)'}
-          subtext={"using YoloV7 and Intel's OpenVino for Charlie to adapt to practically any situtation, outside on the streets or inside the home."}
-          orientation='right'
-        />
-
-        {/* it's called a content explosion because of how ridiculously large the text is */}
-        <section className='content-explosion'>
-          <p className='content-explosion-header'>Service Dogs can up to $50,000 and take two years to train</p>
-          <p className='content-explosion-subtext'>Compare that to Charlie, who has a price point of around <mark>$10,000 or less</mark></p>
-        </section>
-
-        <section className='content-explosion-two'>
-          <img alt='united nations' src={UnitedNations} />
-
-          <div className='content-explosion-two-text'>
-            <p className='content-explosion-two-text-header'>And Here's the Impact.</p>
-            <p className='content-explosion-two-text-subtext'>United Nations goals three and ten, Health and Well-Being, and Reducing Inequalities.</p>
-          </div>
-        </section>
-
-        <section className='content-demo'>
-
-          <div className='content-demo-text'>
-            <p className='content-demo-text-header'>Still not convinced?</p>
-            <p className='content-demo-text-subtext'>Watch our in-action demonstration video!</p>
-          </div>
-
-          <video src={CharlieDemo} controls />
-        </section>
-
-        <section className='content-purchase'>
-          <div className='content-purchase-branding'>
-            <img className='content-purchase-branding-image' alt='charlie-logo' src={CharlieLogo} />
+      <div className='main-container'>
+        <section className='header'>  
+          <div className='header-branding'>
+            <img className='header-image' alt='charlie-logo' src={CharlieLogo} />
             <p>Here-Charlie</p>
           </div>
-
-          <a className='content-purchase-button' href='https://here-charlie.github.io'>Order Now</a>
+          <img className='header-image' alt='github logo' src={Github} />
         </section>
-      </main>
 
-      <footer>
-        <p>©2024 Here-Charlie Group All rights reserved.</p>
-      </footer>
+        <main className='content'>
+
+          <section className='content-hero'>
+            <div className='content-hero-text'>
+              <p className='content-hero-text-billboard'>Venture Outwards with A Robotic Service Dog</p>
+              <p className='content-hero-text-subtext'>endorsed by Intel</p>
+            </div>
+
+            <img alt='charlie' src={Charlie} />
+          </section>
+
+          <Announcement
+            header="What's so special about Charlie?"
+            subtext="Our robotic service dog, Charlie, is built to assist those that are visually impaired. Charlie aims to regain functions that were lost due to visual impairment and be affordable." 
+          />
+
+          <Feature 
+            image={CharlieDetection}
+            header={"Equipped With Lidar and Programmed with ROS"}
+            subtext={"so you know he's quick. Charlie can detect still and moving objects at blazing speeds, accurately."}
+          />
+
+          <Feature 
+            image={OpenVino}
+            header={'Trained From Over 10,000 Pictures (and counting)'}
+            subtext={"using YoloV7 and Intel's OpenVino for Charlie to adapt to practically any situtation, outside on the streets or inside the home."}
+            orientation='right'
+          />
+
+          {/* it's called a content explosion because of how ridiculously large the text is */}
+          <section className='content-explosion'>
+            <p className='content-explosion-header'>Service Dogs can up to $50,000 and take two years to train</p>
+            <p className='content-explosion-subtext'>Compare that to Charlie, who has a price point of around <mark>$10,000 or less</mark></p>
+          </section>
+
+          <section className='content-explosion-two'>
+            <img alt='united nations' src={UnitedNations} />
+
+            <div className='content-explosion-two-text'>
+              <p className='content-explosion-two-text-header'>And Here's the Impact.</p>
+              <p className='content-explosion-two-text-subtext'>United Nations goals three and ten, Health and Well-Being, and Reducing Inequalities.</p>
+            </div>
+          </section>
+
+          <section className='content-demo'>
+
+            <div className='content-demo-text'>
+              <p className='content-demo-text-header'>Still not convinced?</p>
+              <p className='content-demo-text-subtext'>Watch our in-action demonstration video!</p>
+            </div>
+
+            <video src={CharlieDemo} controls />
+          </section>
+
+          <section className='content-purchase'>
+            <div className='content-purchase-branding'>
+              <img className='content-purchase-branding-image' alt='charlie-logo' src={CharlieLogo} />
+              <p>Here-Charlie</p>
+            </div>
+
+            <a className='content-purchase-button' href='https://here-charlie.github.io'>Order Now</a>
+          </section>
+        </main>
+
+        <footer>
+          <p>©2024 Here-Charlie Group All rights reserved.</p>
+        </footer>
+      </div>
     </div>
   )
 }
--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,14 +1,32 @@
 import React from 'react'
 
 export default function Feature({ image, header, subtext, orientation='left' }) {
-  return (
-    <div className='feature'>
-        <img className='feature-image' alt='feature' src={image} />
 
-        <section className='feature-text'>
-            <p className='feature-text-header'>{header}</p>
-            <p className='feature-text-subtext'>{subtext}</p>
-        </section>
-    </div>
+
+
+  return (
+    <>    
+      { orientation === "left" && 
+      <div className='feature'>
+          <img className='feature-image' alt='feature' src={image} />
+
+          <section className='feature-text'>
+              <p className='feature-text-header'>{header}</p>
+              <p className='feature-text-subtext'>{subtext}</p>
+          </section>
+      </div>
+      }  
+
+      { orientation === "right" && 
+      <div className='feature'>
+          <section className='feature-text'>
+              <p className='feature-text-header'>{header}</p>
+              <p className='feature-text-subtext'>{subtext}</p>
+          </section>
+
+          <img className='feature-image' alt='feature' src={image} />
+      </div>
+      }  
+    </>  
   )
 }
